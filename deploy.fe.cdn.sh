329bf20a59d83d0e6746883fdbe58479d8abddf4
tao.xu <tao.xu@qunar.com>
2018-06-03 15:02:35 +0800

    add cdn deploy.sh

---
+++ deploy.fe.cdn.sh
@@ -0,0 +1,4 @@
+mv ./dist/*.css ./dist/*.js ./
+mv ./dist/*html ./views/page
+rm -fr ./dist
+echo 'fe cdn deployed!!'
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 /home/xutao/.nvm/versions/node/v10.2.1/bin/pm2 delete prd
 /home/xutao/.nvm/versions/node/v10.2.1/bin/pm2 start ./conf.prd.json
-echo 'ok'
+echo '后端项目部署ok'
